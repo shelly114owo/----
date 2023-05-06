e0585229e51f2ae81a362d82cb266e66eeb848bc
shelly114owo <119270781+shelly114owo@users.noreply.github.com>
2023-05-06 17:42:02 +0800

    Update action.php

--- website/action.php
+++ website/action.php
@@ -1,4 +1,5 @@
 <a href = "index.php">回到首頁</a> <p>
+
 <?php
 	if(isset($_POST["MyHead"])) {
 		$MyHead=$_POST["MyHead"];
@@ -14,11 +15,16 @@
 		$result = mysqli_query($conn, $sql) or die('MySQL query error');
 		$sql_1 = "SELECT stud_id ,stud_name	,dept_name ,tot_cred FROM student_table where stud_id LIKE \"".$MyHead."%\";";
 		$result_1 = mysqli_query($conn, $sql_1) or die('MySQL query error');
+		echo "<p>學號、名字、部別、總學分</p>";
 		if($row_1 = mysqli_fetch_array($result_1)){
 			echo "<table>","<thead>","<tr>",$row_1['stud_id']," ",$row_1['stud_name']," ",$row_1['dept_name']," ",$row_1['tot_cred']."<p>";
 		}
+		echo "<p>課號、課程名稱、開課系所、年級、必/選修別、目前選修人數、修課人數上限、開課時段</p>";
 		while($row = mysqli_fetch_array($result)){
-			echo "<table>","<thead>","<tr>",$row['course_id']," ",$row['title']," ",$row['dept_name']," ",$row['grade']," ",$row['category']," ",$row['now_people']," ",$row['max_people']."<p>";
+			if($row['category'] == "必修")
+				echo "<table>","<thead>","<tr>","<button type='button' action='action1.php'>申請</button>","<tr>"," ",$row['course_id'],"<tr>","	",$row['title'],"<tr>"," ",$row['dept_name'],"<tr>"," ",$row['grade'],"<tr>"," ",$row['category'],"<tr>"," ",$row['now_people'],"<tr>"," ",$row['max_people']."<p>";
+			else
+				echo "<table>","<thead>","<tr>","<button type='button' action='action1.php'>退選</button>","<tr>"," ",$row['course_id'],"<tr>","	",$row['title'],"<tr>"," ",$row['dept_name'],"<tr>"," ",$row['grade'],"<tr>"," ",$row['category'],"<tr>"," ",$row['now_people'],"<tr>"," ",$row['max_people']."<p>";
 		}
 	}
 ?>
