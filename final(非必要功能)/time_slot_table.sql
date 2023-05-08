d43f4ed136e2adb7397d9815c065065d14632116
shelly114owo <119270781+shelly114owo@users.noreply.github.com>
2023-05-08 20:33:13 +0800

    Add files via upload

---
+++ final(非必要功能)/time_slot_table.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2023-05-08 14:18:12
+-- 伺服器版本： 10.4.28-MariaDB
+-- PHP 版本： 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `new`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `time_slot_table`
+--
+
+CREATE TABLE `time_slot_table` (
+  `time_slot_id` varchar(12) DEFAULT NULL,
+  `day` varchar(3) DEFAULT NULL,
+  `start_time` varchar(10) DEFAULT NULL,
+  `end_time` varchar(8) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- 傾印資料表的資料 `time_slot_table`
+--
+
+INSERT INTO `time_slot_table` (`time_slot_id`, `day`, `start_time`, `end_time`) VALUES
+('(一)01-03', '(一)', '1', '3'),
+('(一)03-04', '(一)', '3', '4'),
+('(一)06', '(一)', '6', '6'),
+('(一)07', '(一)', '7', '7'),
+('(一)08-10', '(一)', '8', '10'),
+('(一)09-10', '(一)', '9', '10'),
+('(二)03-04', '(二)', '3', '4'),
+('(二)06', '(二)', '6', '6'),
+('(二)06-07', '(二)', '6', '7'),
+('(二)07', '(二)', '7', '7'),
+('(二)08-09', '(二)', '8', '9'),
+('(二)10-11', '(二)', '10', '11'),
+('(二)11-12', '(二)', '11', '12'),
+('(二)11-13', '(二)', '11', '13'),
+('(三)03-04', '(三)', '3', '4'),
+('(三)06-07', '(三)', '6', '7'),
+('(三)08-09', '(三)', '8', '9'),
+('(三)09-10', '(三)', '9', '10'),
+('(三)11-13', '(三)', '11', '13'),
+('(五)01-04', '(五)', '1', '4'),
+('(五)02-04', '(五)', '2', '4'),
+('(六)03-04', '(六)', '3', '4'),
+('(四)01', '(四)', '1', '1'),
+('(四)02', '(四)', '2', '2'),
+('(四)03', '(四)', '3', '3'),
+('(四)03-04', '(四)', '3', '4'),
+('(四)04', '(四)', '4', '4'),
+('(四)06', '(四)', '6', '6'),
+('(四)09-10', '(四)', '9', '10'),
+('(四)07-08', '(四)', '7', '8');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
